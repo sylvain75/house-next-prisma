cd63246f562b519cc2b7651d350b74991e21e786
Sylvain <sylvoz75@gmail.com>
2021-02-27 14:30:59 +1100

    firebase initialization

--- src/components/firebaseAuth.tsx
+++ src/components/firebaseAuth.tsx
@@ -1,6 +1,72 @@
-// import { FunctionComponent, useState, useEffect } from "react";
-// import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-// import firebase from "firebase/app";
-// import "firebase/auth";
+import {
+  useEffect,
+  useState,
+  useContext,
+  createContext,
+  FunctionComponent,
+} from "react";
+import { useRouter } from "next/router";
+import firebase from "firebase/app";
+import "firebase/auth";
+import initFirebase from "./initFirebase";
+import { removeTokenCookie, setTokenCookie } from "./tokenCookies";
 
-export {};
+initFirebase();
+
+interface IAuthContext {
+  user: firebase.User | null;
+  logout: () => void;
+  authenticated: boolean;
+}
+
+const AuthContext = createContext<IAuthContext>({
+  user: null,
+  logout: () => null,
+  authenticated: false,
+});
+
+export const AuthProvider: FunctionComponent = ({ children }) => {
+  const [user, setUser] = useState<firebase.User | null>(null);
+  const router = useRouter();
+
+  const logout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        router.push("/");
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
+
+  useEffect(() => {
+    const cancelAuthListener = firebase
+      .auth()
+      .onIdTokenChanged(async (user) => {
+        if (user) {
+          const token = await user.getIdToken();
+          setTokenCookie(token);
+          setUser(user);
+        } else {
+          removeTokenCookie();
+          setUser(null);
+        }
+      });
+
+    return () => {
+      cancelAuthListener();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, logout, authenticated: !!user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
